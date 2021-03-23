39ba84cc5d78652eae521ae57cd4a069cd7e94fc
Georgi L <70279776+GeorgiL410@users.noreply.github.com>
2021-03-23 11:49:29 +0200

    add header and nav item components

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,13 @@
 import logo from './logo.svg';
 import style from './App.module.css';
+import Header from './components/Header/Header';
+import Menu from './components/Menu/Menu'
 
 function App() {
   return (
     <div className={style.app}>
 
-      <nav className={style.navigation}>
-        <ul>
-          <li className="listItem"><img src="/white-origami-bird.png" alt="" /></li>
-          <li className="listItem"><a href="#">Going to 1</a></li>
-          <li className="listItem"><a href="#">Going to 2</a></li>
-          <li className="listItem"><a href="#">Going to 3</a></li>
-          <li className="listItem"><a href="#">Going to 4</a></li>
-          <li className="listItem"><a href="#">Going to 5</a></li>
-          <li className="listItem"><a href="#">Going to 6</a></li>
-          <li className="listItem"><a href="#">Going to 7</a></li>
-          <li className="listItem"><a href="#">Going to 8</a></li>
-          <li className="listItem"><a href="#">Going to 9</a></li>
-          <li className="listItem"><a href="#">Going to 10</a></li>
-          <li className="listItem"><a href="#">Going to 11</a></li>
-        </ul>
-
-
-      </nav>
+     <Header />
     </div>
   );
 }
